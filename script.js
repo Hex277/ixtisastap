31ebf4af138f401c4cb36c85ea7a922d6faba959
Hex CG <131471727+Hex277@users.noreply.github.com>
2025-05-26 14:42:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -257,13 +257,6 @@ function toggleMore(link) {
 }
 const scrollTopBtn = document.getElementById("scrollTopBtn");
 
-window.addEventListener("scroll", () => {
-  if (window.innerWidth <= 768 && window.scrollY > 200) {
-    scrollTopBtn.classList.add("show");
-  } else {
-    scrollTopBtn.classList.remove("show");
-  }
-});
 
 scrollTopBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
