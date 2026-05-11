f5e1dac01fdf88a61d05860e7dae69f1f199d73b
Hex CG <131471727+Hex277@users.noreply.github.com>
2026-05-11 21:01:51 +0400

    Add files via upload

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
+import globals from "globals";
+
 export default [
     {
-        files: ["**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.browser, // document, window və s. tanıması üçün
+                ...globals.node
+            }
+        },
         rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "error"
+            "no-unused-vars": "off", // İstifadə olunmayan dəyişənləri görməzdən gəl (çünki HTML-dən çağırılırlar)
+            "no-undef": "warn",      // Tapılmayan dəyişənləri xəta yox, xəbərdarlıq et
+            "no-console": "off"      // console.log-lara icazə ver
         }
     }
 ];
